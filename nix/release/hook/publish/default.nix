e4c00e42b37b364cf8e1e5f3df2648126addff6b
David Meister <thedavidmeister@gmail.com>
2019-10-17 14:39:27 +1100

    WIP on pulling from core

---
+++ nix/release/hook/publish/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, config }:
+let
+ name = "ct-release-hook-publish";
+
+ script = pkgs.writeShellScriptBin name ''
+set -euox pipefail
+echo "packaging for crates.io"
+# order is important here due to dependencies
+for crate in \
+ crypto_api \
+ detach \
+ sodium \
+ zombie_actor \
+ ghost_actor \
+ lib3h_protocol \
+ mdns \
+ p2p_protocol \
+ lib3h
+do
+ cargo publish --manifest-path "crates/$crate/Cargo.toml"
+
+ sleep 10
+done
+'';
+in
+{
+ buildInputs = [ script ];
+}
